f8504b57a7c902dc46b85e7d576b5f4b875de52c
Soumo Saha <soumosaha4327@gmail.com>
2026-01-14 22:47:58 +0530

    piece length and hashes

--- cmd/torrents/main.go
+++ cmd/torrents/main.go
@@ -9,50 +9,65 @@ import (
 )
 
 func main() {
-	data, _ := os.ReadFile("/Users/szoumo/Downloads/big-buck-bunny.torrent")
-	// Process the data here
-	announce, length, info, err := ParseTorrent(data)
+	data, err := os.ReadFile("/Users/szoumo/Downloads/big-buck-bunny.torrent")
 	if err != nil {
-		fmt.Println("Error parsing torrent:", err)
-		return
+		panic(err)
 	}
 
-	fmt.Println("Announce URL:", announce)
-	fmt.Println("File Length:", length)
-	fmt.Println("Info Dictionary:", info)
+	announce, length, infoHash, info, err := ParseTorrent(data)
+	if err != nil {
+		panic(err)
+	}
 
+	pieceLength, hashes, err := extractPieces(info)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Tracker URL:", announce)
+	fmt.Println("Length:", length)
+	fmt.Println("Info Hash:", infoHash)
+	fmt.Println("Piece Length:", pieceLength)
+	fmt.Println("Piece Hashes:")
+	for _, h := range hashes {
+		fmt.Println(h)
+	}
 }
 
-func ParseTorrent(data []byte) (string, int, string, error) {
+func ParseTorrent(data []byte) (string, int, string, map[string]interface{}, error) {
 	val, _, err := bencode.Decode(data)
 	if err != nil {
-		return "", 0, "", err
+		return "", 0, "", nil, err
 	}
 
 	root, ok := val.(map[string]interface{})
 	if !ok {
-		return "", 0, "", fmt.Errorf("root is not dictionary")
+		return "", 0, "", nil, fmt.Errorf("root is not dictionary")
 	}
 
 	announceBytes, ok := root["announce"].([]byte)
 	if !ok {
-		return "", 0, "", fmt.Errorf("announce missing")
+		return "", 0, "", nil, fmt.Errorf("announce missing")
 	}
 
 	info, ok := root["info"].(map[string]interface{})
 	if !ok {
-		return "", 0, "", fmt.Errorf("info missing")
+		return "", 0, "", nil, fmt.Errorf("info missing")
 	}
-	infoBytes, _ := bencode.Encode(info)
+
+	infoBytes, err := bencode.Encode(info)
+	if err != nil {
+		return "", 0, "", nil, err
+	}
 	h := sha1.Sum(infoBytes)
 	infoHash := hex.EncodeToString(h[:])
 
 	length, err := extractLength(info)
 	if err != nil {
-		return "", 0, "", fmt.Errorf("length missing")
+		return "", 0, "", nil, err
 	}
 
-	return string(announceBytes), length, infoHash, nil
+	return string(announceBytes), length, infoHash, info, nil
 }
 
 func extractLength(info map[string]interface{}) (int, error) {
@@ -64,7 +79,7 @@ func extractLength(info map[string]interface{}) (int, error) {
 	// multi-file torrent
 	filesRaw, ok := info["files"].([]interface{})
 	if !ok {
-		return 0, fmt.Errorf("no length or files field in info")
+		return 0, fmt.Errorf("no length or files field")
 	}
 
 	total := 0
@@ -83,4 +98,27 @@ func extractLength(info map[string]interface{}) (int, error) {
 	}
 
 	return total, nil
+}
+
+func extractPieces(info map[string]interface{}) (int, []string, error) {
+	pieceLength, ok := info["piece length"].(int)
+	if !ok {
+		return 0, nil, fmt.Errorf("piece length missing")
+	}
+
+	piecesRaw, ok := info["pieces"].([]byte)
+	if !ok {
+		return 0, nil, fmt.Errorf("pieces missing")
+	}
+
+	if len(piecesRaw)%20 != 0 {
+		return 0, nil, fmt.Errorf("invalid pieces length")
+	}
+
+	hashes := make([]string, 0, len(piecesRaw)/20)
+	for i := 0; i < len(piecesRaw); i += 20 {
+		hashes = append(hashes, hex.EncodeToString(piecesRaw[i:i+20]))
+	}
+
+	return pieceLength, hashes, nil
 }
